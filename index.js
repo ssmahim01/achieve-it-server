a924f0047a4e69de2689194dd0ba46e5f65b725e
ssmahim01 <mahimzayn666@gmail.com>
2024-12-18 20:33:16 +0600

    Added all files for server side of Achieve-IT

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ybs8l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const courseCollection = client.db("coursesDB").collection("courses");
+
+    app.get("/courses", async(req, res) => {
+        const findAll = courseCollection.find();
+        const result = await findAll.toArray();
+        res.send(result);
+    });
+
+    app.post("/add-course", async(req, res) => {
+        const newCourse = req.body;
+        const result = await courseCollection.insertOne(newCourse);
+        console.log(result);
+        res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from Achieve IT Server....');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
